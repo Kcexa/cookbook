6dbff5d7769a75b45e1f451b9c618125f82d4697
Kseniia <ksushaa22@gmail.com>
2018-10-24 16:18:58 +0700

    add documentation packages and runs
    react-styleguidist & typedoc

---
+++ styleguide.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    webpackConfig: require('./webpack.config.js'),
+    getExampleFilename(componentPath) {
+        return componentPath.replace(/\.tsx?$/, '.examples.md');
+    },
+    propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
+    styleguideDir: "docs/ReactComponents"
+};
