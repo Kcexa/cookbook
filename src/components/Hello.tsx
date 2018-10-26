bbd740bd9586b4178701921cfc4b8cb75209d256
Kseniia <ksushaa22@gmail.com>
2018-10-26 12:28:30 +0700

    add jest for unit testing

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -6,14 +6,27 @@ export interface HelloProps {
 }
 
 /**
+ * Adds two numbers
+ * @function
+ * @param a
+ * @param b
+ */
+export const sum = (a: number, b: number) => {
+    return a + b;
+};
+
+/**
  * Test component
  * @param props
  * @constructor
  */
 const Hello = (props: HelloProps) => (
-    <h1>
-        Hello from {props.compiler} and {props.framework}!
-    </h1>
+    <>
+        <h1>
+            Hello from {props.compiler} and {props.framework}!
+        </h1>
+        <h3>I can add numbers. Check it out: 1 + 2 = {`${sum(1, 2)}`}</h3>
+    </>
 );
 
 export default Hello;
